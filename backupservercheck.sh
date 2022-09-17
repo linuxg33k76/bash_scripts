ae305fd69d6a7fc957640f738818b24cb665bcbd
Ben C <linuxg33k76@gmail.com>
2022-09-16 23:09:26 -0600

    Add files via upload

---
+++ backupservercheck.sh
@@ -0,0 +1,18 @@
+#!/bin/env bash
+
+# Backup Check - checks for backup server and alerts user
+# Created by Ben Calvert on 16 Sept 2022
+
+BACKUPSERVER="fedora-macmini"
+
+
+# Mask all output of the ping command to hide extra terminal content
+ping -c 1 $BACKUPSERVER 1> /dev/null 2> /dev/null
+
+# Test for successful command execution
+if [ $? = "0" ]; then
+  echo "Backup Server is ONLINE"
+else
+  echo "Backup Server is OFFLINE"
+fi
+
