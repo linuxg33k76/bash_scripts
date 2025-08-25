50afa205a1aa4918eed5038d0358d049e4b4b97b
Ben Calvert <macg33k@proton.me>
2025-08-24 21:33:01 -0600

    updated move script to prompt for usernames and check for sudo execution

--- move_home_data.sh
+++ move_home_data.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 # move_home_data.sh
 
-# Set your old and new usernames here
-OLDUSER="oldname"
-NEWUSER="newname"
+# Check to see if script is run as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (e.g., sudo $0)"
+  exit 1
+fi  
+
+# Ask for old and new usernames
+echo 
+read -p "Enter old username: " OLDUSER
+echo
+read -p "Enter new username: " NEWUSER
 
 OLDHOME="/Users/$OLDUSER"
 NEWHOME="/Users/$NEWUSER"
 
+echo
 echo "Moving files from $OLDHOME to $NEWHOME..."
+
+# Press Enter to continue
+echo
+read -p "Press Enter to continue... (or Ctrl+C to cancel)"
 
 # Move regular files and folders
 sudo mv "$OLDHOME"/* "$NEWHOME"/
